a10647a62adc7a658f5cf7253e2a53c4a9e44bef
Humberto Smith da Silva <93011482+humbsmith84@users.noreply.github.com>
2024-10-22 19:18:25 -0300

    asdasdasd

--- src/script/script.js
+++ src/script/script.js
@@ -2,4 +2,8 @@ document.querySelectorAll('.kaban-card').forEach(card => {
     card.addEventListener('dragstart', e => {
         e.currentTarget.classList.add('dragging');
     })
+
+    card.addEventListener('dragend', e => {
+        e.currentTarget.classList.remove('dragging');
+    })
 })
